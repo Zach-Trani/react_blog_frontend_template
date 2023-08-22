9393928e4cdacd2b9eec21fc89480f94b86c0ad3
Zach-Trani <trani.zach@gmail.com>
2023-08-22 14:02:24 -0400

    Fix: react bootstrap imports

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -1,32 +1,55 @@
-import React, {useState} from 'react'
-import axios from 'axios'
-import { Navigate, useNavigate } from 'react-router-dom'
-import { Form, Button, Container } from 'react-router-dom'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Container } from "react-bootstrap";
 
 const NewPostPage = () => {
+  // form has a default state of empty
+  const [post, setPost] = useState({
+    title: "",
+    author: "",
+    image: "",
+    content: "",
+  });
 
-    // form has a default state of empty 
-    const [post, setPost] = useState({
-        title: '',
-        author: '',
-        image: '',
-        content: ''
-    })
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleChange = (e) => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = e => {
-        setPost({...post, [e.target.name]: e.target.value })
-    }
+  // async when using the api (backend)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5500/posts", post);
+    navigate("/");
+  };
 
-    // async when using the api (backend)
-     const handleSubmit = async e => {
-        e.preventDefault()
-        await axios.post('http://localhost:5500/posts', post)
-        navigate('/')
-     }
+  return (
+    <Container className="mt-4">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.label>Title</Form.label>
+          <Form.Control type='text' name='title' placeholder='Title' onChange={handleChange} />
+        </Form.Group>
+        <Form.Group>
+          <Form.label>Author</Form.label>
+          <Form.Control type='text' name='author' placeholder='Author' onChange={handleChange} />
+         </Form.Group>
+        <Form.Group>
+          <Form.label>Image URL</Form.label>
+          <Form.Control type='text' name='image' placeholder='Image URL' onChange={handleChange} />
+        </Form.Group>
+        <Form.Group>
+          <Form.label>Content</Form.label>
+          <Form.Control type='text' name='content' rows={5} placeholder='Content' onChange={handleChange} />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+            Create
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
-     return(
-        
-     )
-}
+export default NewPostPage;
